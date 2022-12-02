a14b44d5c6c93f458dd1c6c49f29777655cc110e
Chengro123 <chen2000hongrui@hotmail.com>
2022-12-02 13:46:58 +0100

    better solution for day2

--- day02/day02.py
+++ day02/day02.py
@@ -1,16 +1,12 @@
 with open('input.txt') as line:
-    l = [x.strip('\n') for x in line]
+    matchhistory = [x.strip('\n') for x in line]
 
+    # Part 1
     result1 = {'A X':4, 'A Y':8, 'A Z':3, 'B X':1, 'B Y':5, 'B Z':9, 'C X':7, 'C Y':2, 'C Z':6}    
-    points1 = 0
-    for x in l:
-        points = points + result1[x]
-    
-    print(points)
+    points1 = sum([result1[match] for match in matchhistory])
+    print(points1)
 
+    # Part 2 
     result2 = {'A X':3, 'A Y':4, 'A Z':8, 'B X':1, 'B Y':5, 'B Z':9, 'C X':2, 'C Y':6, 'C Z':7} 
-    points2 = 0 
-    for x in l:
-        points2 = points2 + result2[x]
-    
+    points2 = sum([result2[match] for match in matchhistory])
     print(points2)
